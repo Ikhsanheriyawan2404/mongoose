6ee160575cd342d045e0961985de641b746a60fa
ikhsanheriyawan2404 <ikhsanheriyawan2404@gmail.com>
2022-07-05 19:51:27 +0700

    setup layouts

--- app.js
+++ app.js
@@ -1,9 +1,33 @@
-const express = require('express');
-const expresLayouts = require('express-ejs-layouts');
+const express = require('express')
+const expresLayouts = require('express-ejs-layouts')
 
-const app = express();
-const port = 1234;
+const app = express()
+const port = 1234
+
+app.set('view engine', 'ejs')
+app.use(expresLayouts)
+app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true}))
+
+app.get('/', (req, res) => {
+    res.render('index', {
+        layout: 'layouts/main',
+        title: 'Halaman Home',
+    });
+});
+
+// Read Contacts
+app.get('/contact', (req, res) => {
+    // const data = contacts.loadContacts();
+    res.render('contacts/index', {
+        layout: 'layouts/main',
+        title: 'Halaman Contact',
+        // data,
+        // msg: req.flash('msg')
+    });
+});
 
 app.listen(port, () => {
-    console.log(`Mongoose Contact App | Listening at http://localhost:${port}`);
+    console.log(`Mongoose Contact App | Listening at http://localhost:${port}`)
 })
+
