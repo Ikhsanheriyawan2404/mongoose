1ca7efe3e66b3db887798f3f7799be0c8bb8ef1e
ikhsanheriyawan2404 <ikhsanheriyawan2404@gmail.com>
2022-07-06 10:10:54 +0700

    feat: delete data contacts

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const express = require('express')
 const expresLayouts = require('express-ejs-layouts')
+const { body, validationResult, check } = require('express-validator')
+const methodOverride = require('method-override')
+
 
 require('./utils/db')
 const Contact = require('./models/contact')
@@ -11,6 +14,7 @@ const flash = require('connect-flash');
 const app = express()
 const port = 1234
 
+app.use(methodOverride('_method'))
 app.set('view engine', 'ejs')
 app.use(expresLayouts)
 app.use(express.static('public'))
@@ -37,11 +41,54 @@ app.get('/contact', async (req, res) => {
     const data = await Contact.find();
     res.render('contacts/index', {
         layout: 'layouts/main',
-        title: 'Halaman Contact',
+        title: 'Contact',
         data,
         msg: req.flash('msg')
     });
 });
+
+// View Create Contacts
+app.get('/contact/create', async (req, res) => {
+    res.render('contacts/create', {
+        layout: 'layouts/main',
+        title: 'Create Contact',
+        msg: req.flash('msg')
+    });
+});
+
+// Create Contacts
+app.post('/contact', [
+    body('name').custom(async (value) => {
+        const duplicate = await Contact.findOne({ name: value })
+        if (duplicate) {
+            throw new Error('NIK sudah terdaftar!');
+        }
+        return true;
+    }),
+    body('name').isLength({ min: 3 })
+],
+    (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        res.render('contacts/create', {
+            layout: 'layouts/main',
+            title: 'Form Tambah Kontak',
+            errors: errors.array()
+        })
+    } else {
+        Contact.insertMany(req.body, (error, result) => {
+            req.flash('msg', 'Data kontak berhasil ditambahkan!');
+            res.redirect('/contact');
+        });
+    }
+});
+
+app.delete('/contact/', (req, res) => {
+    Contact.deleteOne({ _id: req.body._id}).then((result) => {
+        req.flash('msg', 'Data kontak berhasil dihapus!');
+        res.redirect('/contact');
+    });
+})
 
 // // Detail Contact
 // app.get('/contact/', async (req, res) => {
